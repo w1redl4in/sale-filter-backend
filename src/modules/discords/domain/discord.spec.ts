ba02082ebc132ccc47cf6cfd55b3df82132f6d70
w1redl4in <felipe.austriaco@mercadolivre.com>
2023-07-08 15:55:44 -0300

    test(discord/domain): :white_check_mark: add tests for discord domain

---
+++ src/modules/discords/domain/discord.spec.ts
@@ -0,0 +1,37 @@
+import { left } from '../../../core/logic/Either';
+import { Discord } from './discord';
+import { InvalidWebhookUrlError } from './errors/invalid-webhook-error';
+
+describe('Discord Domain', () => {
+  const keys = ['4070 Ti', '3060 Ti'];
+  const discordName = 'Discord Server Name';
+  const webhookUrl = 'https://discord.com/mockWebhookUrl';
+
+  it('should be able to create a Discord instance when data is valid', () => {
+    const discordOrError = Discord.create({
+      webhookUrl,
+      discordName,
+      keys,
+    });
+    expect(discordOrError.isRight()).toBeTruthy();
+  });
+
+  it('should be able to create a Discord instance without keys', () => {
+    const discordOrError = Discord.create({
+      webhookUrl,
+      discordName,
+    });
+    expect(discordOrError.isRight()).toBeTruthy();
+  });
+
+  it('shouldnt able to create a Discord without a webhookUrl', () => {
+    const discordOrError = Discord.create({
+      discordName,
+      keys,
+      webhookUrl: '',
+    });
+
+    expect(discordOrError.isLeft()).toBeTruthy();
+    expect(discordOrError).toEqual(left(new InvalidWebhookUrlError('')));
+  });
+});
