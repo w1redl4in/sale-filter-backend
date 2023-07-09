d063f3c77dd802c2b59006643d98cd6ac5be2793
w1redl4in <felipe.austriaco@mercadolivre.com>
2023-07-09 00:18:51 -0300

    chore(infra): :wrench: add db config

--- src/infra/http/app.module.ts
+++ src/infra/http/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BotsModule } from './bots/bots.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from '../db/config';
 
 @Module({
-  imports: [BotsModule],
+  imports: [BotsModule, TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
