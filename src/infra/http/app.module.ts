a8a84150d6b6901477d01583663f913862deee1a
w1redl4in <felipe.austriaco@mercadolivre.com>
2023-07-09 15:07:07 -0300

    chore(infra/http): :wrench: add discord module on nestjs

--- src/infra/http/app.module.ts
+++ src/infra/http/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BotsModule } from './bots/bots.module';
+import { DiscordsModule } from './discords/discords.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from '../db/config';
 
 @Module({
-  imports: [BotsModule, TypeOrmModule.forRoot(config)],
+  imports: [BotsModule, DiscordsModule, TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
